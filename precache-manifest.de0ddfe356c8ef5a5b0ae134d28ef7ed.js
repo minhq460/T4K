d03a471a91c5a055914dfd89f3825b9ee77dc077
Châu Vũ Minh Quân <18130185@st.hcmuaf.edu.vn>
2022-02-18 19:33:49 +0700

    Updates

---
+++ precache-manifest.de0ddfe356c8ef5a5b0ae134d28ef7ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2e9c89968a80e5a06e47cf8e6a1e62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87c27b5830526fb6fb28",
+    "url": "/static/css/main.fe9c0f9d.chunk.css"
+  },
+  {
+    "revision": "51766877bdcb86f8ad3c",
+    "url": "/static/js/2.96196d7b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.96196d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87c27b5830526fb6fb28",
+    "url": "/static/js/main.c9d3c878.chunk.js"
+  },
+  {
+    "revision": "65f8c86f4c8250bd2868",
+    "url": "/static/js/runtime-main.5cdce585.js"
+  }
+]);
